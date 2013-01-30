00e5d0173188dcd2df271351a966b2b64016749a
Hakan Ensari <hakan.ensari@papercavalier.com>
2013-01-30 11:16:01 +0000

    Use Sinatra idiom

--- app.rb
+++ app.rb
@@ -1,13 +1,13 @@
 require 'sinatra'
 require 'sinatra/jsonp'
 
-@@strategies = File.read('config/oblique.txt').split "\n"
+set :strategies => File.read('config/oblique.txt').split("\n")
 
 get '/' do
-  jsonp @@strategies.sample
+  jsonp settings.strategies.sample
 end
 
 get %r{/([1-9]\d*)} do |index|
   cache_control :public
-  jsonp @@strategies.fetch(index.to_i - 1) { halt 400 }
+  jsonp settings.strategies.fetch(index.to_i - 1) { halt 400 }
 end
